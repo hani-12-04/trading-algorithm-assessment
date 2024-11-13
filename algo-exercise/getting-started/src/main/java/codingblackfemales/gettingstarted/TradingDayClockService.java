e8f540b1ec760474121e5fa0c9f10de502fcb02e
Hani Ali <haniali1204@gmail.com>
2024-11-13 14:50:21 +0000

    v1

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TradingDayClockService.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/TradingDayClockService.java
@@ -22,9 +22,10 @@ public class TradingDayClockService {
      * Checks if the current time has reached or passed the end of the trading day.
      * The end of the trading day is at 5:00 PM (17:00).
      * @return True if the time is 5:00 PM or later; otherwise, false.
+     * currentTime is exactly 5:00 PM (returns 0), or later than 5:00 PM (returns 1)
      */
     public static boolean isEndOfDay() {
-        int currentTimeComparison = currentTime.compareTo(LocalTime.of(17,0)) ;
+        int currentTimeComparison = currentTime.compareTo(LocalTime.of(17,0)) ; //compares currentTime to 5:00 PM
         return currentTimeComparison == 0 ||  currentTimeComparison == 1;
     }
 }
